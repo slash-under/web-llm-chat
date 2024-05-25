7a5297145ab6ada51535d9b73b32fdedb8024f2e
Nestor Qin <imba.qxy@gmail.com>
2024-05-25 02:48:07 -0400

    analytics: collect error reports

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -78,7 +78,7 @@ const en = {
       SaveAs: "Save as Template",
       Confirm: "Confirm",
     },
-    IsContext: "Contextual Prompt",
+    IsContext: "System Prompt",
   },
   Export: {
     Title: "Export Messages",
@@ -402,7 +402,7 @@ const en = {
     Failed: "Download failed.",
   },
   Context: {
-    Toast: (x: any) => `With ${x} contextual prompts`,
+    Toast: (x: any) => `With ${x} system prompts`,
     Edit: "Current Chat Settings",
     Add: "Add a Prompt",
     Clear: "Context Cleared",
